4ef3ebc98a5d00f130d82e8cd818640157e5d293
Twiindileni <150262434+Twiindileni@users.noreply.github.com>
2023-11-10 13:42:18 +0200

    Add files via upload

--- scriptofficerlogin.js
+++ scriptofficerlogin.js
@@ -5,14 +5,22 @@ document.addEventListener("DOMContentLoaded", function () {
         event.preventDefault();
 
         // In a real application, you would make an AJAX request to the server for authentication
-        // For this example, we'll use a hardcoded username and ID for demonstration purposes
+        // For this example, we'll use an array to store user credentials and redirect URLs
+        const users = [
+            { idNumber: "88032500588", password: "1988", redirectUrl: "List.html" },
+            { idNumber: "12345", password: "123", redirectUrl: "List.html" },
+            // Add more users as needed with their corresponding redirect URLs
+        ];
+
         const idNumber = document.getElementById("id-number").value;
         const password = document.getElementById("password").value;
 
-        // Simulate server authentication (replace with your own authentication logic)
-        if (idNumber === "00588" && password === "1988") {
-            // Redirect to the dashboard page
-            window.location.href = `List.html`;
+        // Simulate server authentication by checking against the array
+        const user = users.find(u => u.idNumber === idNumber && u.password === password);
+
+        if (user) {
+            // Redirect to the specified URL for the authenticated user
+            window.location.href = user.redirectUrl;
         } else {
             alert("Invalid credentials. Please try again.");
         }
